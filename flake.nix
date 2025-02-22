f0d41d53dffb8f8471c63e24d34d9e683f94d054
Matthew Holden <Matthew.Holden@spmoilandgas.com>
2025-02-22 16:37:29 -0600

    First commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager Configuration";
+  inputs = {
+    catppuccin.url = "https://flakehub.com/f/catppuccin/nix/1.2.1.tar.gz";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*";
+    home-manager.url = "https://flakehub.com/f/nix-community/home-manager/0.2411.*";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.2411.*";
+  };
+  outputs =
+    { self, ... }@inputs:
+    let
+      inherit (self) outputs;
+      stateVersion = "24.11";
+      helper = import ./lib { inherit inputs outputs stateVersion; };
+    in
+    {
+      homeConfigurations = {
+        "matthewholden@macHostname" = helper.mkHome {
+          username = "matthewholden";
+          hostname = "C002108230";
+        };
+      };
+    };
+}
---
+++ home-manager/default.nix
@@ -0,0 +1,121 @@
+{
+  lib,
+  pkgs,
+  stateVersion,
+  username,
+  ...
+}:
+let
+  inherit (pkgs.stdenv) isDarwin isLinux;
+in
+{
+  home = {
+    inherit stateVersion;
+    inherit username;
+    homeDirectory = if isDarwin then "/Users/${username}" else "/home/${username}";
+
+    catppuccin.enable = true;
+    catpuccin.flavor = "mocha";
+
+    packages =
+      with pkgs;
+      [
+        cpufetch
+        fastfetch
+        ipfetch
+        onefetch
+        micro
+      ]
+      ++ lib.optionals isLinux [
+        ramfetch
+      ]
+      ++ lib.optionals isDarwin [
+        m-cli
+      ];
+    sessionVariables = {
+      EDITOR = "nvim";
+      SYSTEMD_EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+  };
+
+  # Workaround home-manager bug
+  # - https://github.com/nix-community/home-manager/issues/2033
+  news = {
+    display = "silent";
+    entries = lib.mkForce [ ];
+  };
+
+  nix = {
+    package = pkgs.nixVersions.latest;
+  };
+
+  programs = {
+    home-manager.enable = true;
+    micro = {
+      enable = true;
+      settings = {
+        autosu = true;
+        diffgutter = true;
+        paste = true;
+        rmtrailingws = true;
+        savecursor = true;
+        saveundo = true;
+        scrollbar = true;
+        scrollbarchar = "░";
+        scrollmargin = 4;
+        scrollspeed = 1;
+      };
+    };
+
+    bat.enable = true;
+
+    lazygit.enable = true;
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    fd = {
+      enable = true;
+    };
+
+    kubecolor = {
+      enable = true;
+      enableAlias = true;
+    };
+
+    kitty = {
+      enable = true;
+      shellIntegration.enableZshIntegration = true;
+    };
+
+    jq.enable = true;
+
+    zoxide = {
+      enable = true;
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableVteIntegration = true;
+      userFriendlyNames = true;
+      autocd = true;
+      autosuggestion = {
+        enable = true;
+      };
+      completionInit = "autoload -U compinit && compinit";
+      dotDir = ".config/zsh";
+      historySubstringSearch.enable = true;
+      profileExtra = 'eval "$(/opt/homebrew/bin/brew shellenv)"';
+      syntaxHighlighting = {
+        enable = true;
+      };
+      zsh-abbr = {
+        enable = true;
+      };
+    };
+  };
+}
